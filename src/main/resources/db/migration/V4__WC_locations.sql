27b172c18d41025198274a115d9ce44109aad822
thembu <rakhivhanithembu@gmail.com>
2026-05-26 00:47:02 +0200

    added more western cape locations and ux (sound, crosshair cursor , visible distance)

---
+++ src/main/resources/db/migration/V4__WC_locations.sql
@@ -0,0 +1,15 @@
+INSERT INTO locations (name, city, province, difficulty, latitude, longitude) VALUES
+
+                                                                                  ('Touws River', 'Breede Valley Local Municipality', 'WESTERN_CAPE', 'HARD', -33.34696410535644, 20.029770942218818),
+                                                                                  ('Riversdale', 'George', 'WESTERN_CAPE', 'HARD', -34.09328840798149, 21.247570787867016),
+                                                                                  ('Monterey Pines Cres', 'Cape Town', 'WESTERN_CAPE', 'MEDIUM', -33.795631751651705, 18.48545376337656),
+                                                                                  ('Petunia St', 'Swellendam Local Municipality', 'WESTERN_CAPE', 'HARD', -34.04422887071696, 20.44107288302079),
+                                                                                  ('34 M65', 'Scarborough', 'WESTERN_CAPE', 'MEDIUM', -34.20082362426941, 18.379922283131975),
+                                                                                  ('Worcester', 'Breede Valley Local Municipality', 'WESTERN_CAPE', 'HARD', -33.64859966616642, 19.48749311872339),
+                                                                                  ('Mossel Bay,', 'George', 'WESTERN_CAPE', 'EASY', -34.201870091711776, 22.030052723847135),
+                                                                                  ('Merweville', 'Beaufort West Local Municipality', 'WESTERN_CAPE', 'HARD', -32.67020725211342, 21.507164536740362),
+                                                                                  ('Kerk St', 'Cape Agulhas Local Municipality', 'WESTERN_CAPE', 'EASY', -34.59158874576444, 19.759622809746517),
+                                                                                  ('Still Bay', 'George', 'WESTERN_CAPE', 'MEDIUM', -34.366737100874836, 21.417270998513118),
+                                                                                  ('Pringle Bay', 'Overstrand Local Municipality', 'WESTERN_CAPE', 'EASY', -34.35029167983811, 18.81697213014055)
+
+;
---
+++ src/main/resources/db/migration/V5__more_WC_locations.sql
@@ -0,0 +1,17 @@
+INSERT INTO locations (name, city, province, difficulty, latitude, longitude) VALUES
+
+                                                                                  ('Doringbaai', 'Matzikama Local Municipality', 'WESTERN_CAPE', 'HARD', -31.818496132628034, 18.237716014080277),
+                                                                                  ('1 Kerk St', 'Cape Agulhas Local Municipality', 'WESTERN_CAPE', 'EASY', -34.59158874576444, 19.759622809746517),
+                                                                                  ('Still Bay,', 'George', 'WESTERN_CAPE', 'EASY', -34.366737100874836, 21.417270998513118),
+                                                                                  ('Pringle Bay,', 'Overstrand Local Municipality', 'WESTERN_CAPE', 'EASY', -34.35029167983811, 18.81697213014055),
+                                                                                  ('Uniondale', 'George', 'WESTERN_CAPE', 'MEDIUM', -33.656402031337315, 23.12858230251879),
+                                                                                  ('Ceres', 'Witzenberg Local Municipality', 'WESTERN_CAPE', 'HARD', -33.365034389141826, 19.300696737549774),
+                                                                                  ('74 Vaphi Cres', 'Khayelitsha', 'WESTERN_CAPE', 'MEDIUM', -34.028780391191944, 18.6608140665797),
+                                                                                  ('Paternoster', 'Saldanha Bay Local Municipality', 'WESTERN_CAPE', 'EASY', -32.811674329574345, 17.884311019062345),
+                                                                                  ('Die Dorp Op Die Berg', 'Witzenberg Local Municipality', 'WESTERN_CAPE', 'HARD', -33.0221946058251, 19.30922706857985),
+                                                                                  ('3 Pieter Wium St', 'Paarl', 'WESTERN_CAPE', 'EASY', -33.70776092685589, 18.962588901159375),
+                                                                                  ('De Rust', 'George', 'WESTERN_CAPE', 'MEDIUM', -33.48959618508583, 22.532912644848757),
+                                                                                  ('McGregor', 'Langeberg Local Municipality', 'WESTERN_CAPE', 'EASY', -33.94374690883506, 19.83302973684351),
+                                                                                  ('Laingsburg', 'Laingsburg Local Municipality', 'WESTERN_CAPE', 'MEDIUM', -33.194321617606036, 20.85916966970809)
+
+;
